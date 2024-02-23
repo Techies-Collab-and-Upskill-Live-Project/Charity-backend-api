670fdbdc306041c49f70d5c700a0ce520f39991d
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-23 21:23:45 +0100

    fix(api): fixed cors issues

--- charitydonate/settings.py
+++ charitydonate/settings.py
@@ -186,9 +186,9 @@ SPECTACULAR_SETTINGS = {
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:8000', 'http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:8000', 'http://localhost:3000', 'https://charity-donation.onrender.com']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:3000', 'https://charity-donation.onrender.com']
 
 CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
